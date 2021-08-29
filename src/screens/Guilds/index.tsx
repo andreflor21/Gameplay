9d40be10641b3316955dbe8bd49441615a0077c3
Andre Flor <andreflor96@hotmail.com>
2021-08-29 11:49:14 -0300

    fix: add list divider on first item

--- src/screens/Guilds/index.tsx
+++ src/screens/Guilds/index.tsx
@@ -5,35 +5,40 @@ import { ListDivider } from "../../components/ListDivider";
 
 import { styles } from "./styles";
 type Props = {
-  handleGuildSelect: (guild: GuildProps) => void;
+    handleGuildSelect: (guild: GuildProps) => void;
 };
 export const Guilds = ({ handleGuildSelect }: Props) => {
-  const guilds = [
-    {
-      id: "1",
-      name: "lendarios",
-      icon: null,
-      owner: true,
-    },
-    {
-      id: "2",
-      name: "SAS BROS",
-      icon: null,
-      owner: false,
-    },
-  ];
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={guilds}
-        keyExtractor={(intem) => intem.id}
-        renderItem={({ item }) => (
-          <Guild data={item} onPress={() => handleGuildSelect(item)} />
-        )}
-        ItemSeparatorComponent={() => <ListDivider />}
-        showsVerticalScrollIndicator={false}
-        style={styles.guilds}
-      />
-    </View>
-  );
+    const guilds = [
+        {
+            id: "1",
+            name: "lendarios",
+            icon: null,
+            owner: true,
+        },
+        {
+            id: "2",
+            name: "SAS BROS",
+            icon: null,
+            owner: false,
+        },
+    ];
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={guilds}
+                keyExtractor={(intem) => intem.id}
+                renderItem={({ item }) => (
+                    <Guild
+                        data={item}
+                        onPress={() => handleGuildSelect(item)}
+                    />
+                )}
+                ItemSeparatorComponent={() => <ListDivider isCentered />}
+                ListHeaderComponent={() => <ListDivider isCentered />}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{ paddingBottom: 68, paddingTop: 103 }}
+                style={styles.guilds}
+            />
+        </View>
+    );
 };
