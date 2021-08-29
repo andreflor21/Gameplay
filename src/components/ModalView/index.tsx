eead544fbbbbf66d88e3382634fb0ed57038e417
Andre Flor <andreflor96@hotmail.com>
2021-08-29 11:48:36 -0300

    fix: close modal without select any guild

--- src/components/ModalView/index.tsx
+++ src/components/ModalView/index.tsx
@@ -1,23 +1,31 @@
 import React, { ReactNode } from "react";
 
-import { Modal, View, ModalProps } from "react-native";
+import {
+    Modal,
+    View,
+    ModalProps,
+    TouchableWithoutFeedback,
+} from "react-native";
 import { Background } from "../Background";
 import { styles } from "./styles";
 
 type Props = ModalProps & {
-  children: ReactNode;
+    children: ReactNode;
+    closeModal: () => void;
 };
-export const ModalView = ({ children, ...rest }: Props) => {
-  return (
-    <Modal transparent animationType="slide" {...rest}>
-      <View style={styles.overlay}>
-        <View style={styles.container}>
-          <Background>
-            <View style={styles.bar} />
-            {children}
-          </Background>
-        </View>
-      </View>
-    </Modal>
-  );
+export const ModalView = ({ children, closeModal, ...rest }: Props) => {
+    return (
+        <Modal transparent animationType="slide" statusBarTranslucent {...rest}>
+            <TouchableWithoutFeedback onPress={closeModal}>
+                <View style={styles.overlay}>
+                    <View style={styles.container}>
+                        <Background>
+                            <View style={styles.bar} />
+                            {children}
+                        </Background>
+                    </View>
+                </View>
+            </TouchableWithoutFeedback>
+        </Modal>
+    );
 };
