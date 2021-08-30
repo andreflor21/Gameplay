325139dce6a0510754c3f1dd25e033d3230ed427
Andre Flor <andreflor96@hotmail.com>
2021-08-29 22:00:37 -0300

    fix: using prop isCentered

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -14,50 +14,54 @@ import { Member } from "../../components/Member";
 import { ListDivider } from "../../components/ListDivider";
 import { ButtonIcon } from "../../components/ButtonIcon";
 export const AppointmentDetails = () => {
-  const members = [
-    {
-      id: "1",
-      username: "Andre",
-      avatar_url: "https://github.com/andreflor21.png",
-      status: "online",
-    },
-    {
-      id: "2",
-      username: "Andre",
-      avatar_url: "https://github.com/andreflor21.png",
-      status: "offline",
-    },
-  ];
-  return (
-    <Background>
-      <Header
-        title="Detalhes"
-        action={
-          <BorderlessButton>
-            <Fontisto name="share" size={24} color={theme.colors.primary} />
-          </BorderlessButton>
-        }
-      />
-      <ImageBackground source={Banner} style={styles.banner}>
-        <View style={styles.bannerContent}>
-          <Text style={styles.title}>Lendário</Text>
-          <Text style={styles.subtitle}>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae
-            impedit{" "}
-          </Text>
-        </View>
-      </ImageBackground>
-      <ListHeader title="Jogadores" subtitle="Total 3" />
-      <FlatList
-        data={members}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <Member data={item} />}
-        ItemSeparatorComponent={() => <ListDivider />}
-        style={styles.members}
-      />
-      <View style={styles.footer}>
-        <ButtonIcon title="Entrar na partida" />
-      </View>
-    </Background>
-  );
+    const members = [
+        {
+            id: "1",
+            username: "Andre",
+            avatar_url: "https://github.com/andreflor21.png",
+            status: "online",
+        },
+        {
+            id: "2",
+            username: "Andre",
+            avatar_url: "https://github.com/andreflor21.png",
+            status: "offline",
+        },
+    ];
+    return (
+        <Background>
+            <Header
+                title="Detalhes"
+                action={
+                    <BorderlessButton>
+                        <Fontisto
+                            name="share"
+                            size={24}
+                            color={theme.colors.primary}
+                        />
+                    </BorderlessButton>
+                }
+            />
+            <ImageBackground source={Banner} style={styles.banner}>
+                <View style={styles.bannerContent}>
+                    <Text style={styles.title}>Lendário</Text>
+                    <Text style={styles.subtitle}>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Vitae impedit{" "}
+                    </Text>
+                </View>
+            </ImageBackground>
+            <ListHeader title="Jogadores" subtitle="Total 3" />
+            <FlatList
+                data={members}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) => <Member data={item} />}
+                ItemSeparatorComponent={() => <ListDivider isCentered />}
+                style={styles.members}
+            />
+            <View style={styles.footer}>
+                <ButtonIcon title="Entrar na partida" />
+            </View>
+        </Background>
+    );
 };
