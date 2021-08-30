2827ba2190fbbb17ea821382c5cf1566592d8e56
Andre Flor <andreflor96@hotmail.com>
2021-08-29 21:24:25 -0300

    fix: private and public routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { AuthRoutes } from "./auth.routes";
-
+import { useAuth } from "../hooks/auth";
+import { SignIn } from "../screens/SignIn";
 export const Routes = () => {
-  return (
-    <NavigationContainer>
-      <AuthRoutes />
-    </NavigationContainer>
-  );
+    const { user } = useAuth();
+    return (
+        <NavigationContainer>
+            {user.id ? <AuthRoutes /> : <SignIn />}
+        </NavigationContainer>
+    );
 };
