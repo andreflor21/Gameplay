91c1f7679d65a612d833bda39fc14e8e76cf6322
Andre Flor <andreflor96@hotmail.com>
2021-08-29 21:23:08 -0300

    feat:  auth provider

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,38 +1,52 @@
 import React from "react";
-import { Image, Text, View } from "react-native";
+import { Image, Text, View, Alert, ActivityIndicator } from "react-native";
+
+import { theme } from "../../global/styles/theme";
 import { styles } from "./styles";
 import IllustrationImg from "../../assets/illustration.png";
+
 import { Background } from "../../components/Background";
-
 import { ButtonIcon } from "../../components/ButtonIcon";
-import { useNavigation } from "@react-navigation/native";
+
+import { useAuth } from "../../hooks/auth";
+
 export function SignIn() {
-  const navigation = useNavigation();
+    const { loading, signIn } = useAuth();
+    const HandleSingIn = async () => {
+        try {
+            await signIn();
+        } catch (error) {
+            Alert.alert(error);
+        }
+    };
+    return (
+        <Background>
+            <View style={styles.container}>
+                <Image
+                    source={IllustrationImg}
+                    style={styles.image}
+                    resizeMode="stretch"
+                />
 
-  const HandleSingIn = () => {
-    navigation.navigate("Home");
-  };
-  return (
-    <Background>
-      <View style={styles.container}>
-        <Image
-          source={IllustrationImg}
-          style={styles.image}
-          resizeMode="stretch"
-        />
-
-        <View style={styles.content}>
-          <Text style={styles.title}>
-            Conecte-se {"\n"}e organize {"\n"}
-            suas jogatinas
-          </Text>
-          <Text style={styles.subtilte}>
-            Crie grupos para jogar seus games {"\n"}
-            favoritos com seus amigos
-          </Text>
-          <ButtonIcon onPress={HandleSingIn} title="Entrar com Discord" />
-        </View>
-      </View>
-    </Background>
-  );
+                <View style={styles.content}>
+                    <Text style={styles.title}>
+                        Conecte-se {"\n"}e organize {"\n"}
+                        suas jogatinas
+                    </Text>
+                    <Text style={styles.subtilte}>
+                        Crie grupos para jogar seus games {"\n"}
+                        favoritos com seus amigos
+                    </Text>
+                    {loading ? (
+                        <ActivityIndicator color={theme.colors.primary} />
+                    ) : (
+                        <ButtonIcon
+                            onPress={HandleSingIn}
+                            title="Entrar com Discord"
+                        />
+                    )}
+                </View>
+            </View>
+        </Background>
+    );
 }
