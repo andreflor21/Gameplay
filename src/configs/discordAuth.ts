a967e8d5506da03ad1873026000f3ee378855814
Andre Flor <andreflor96@hotmail.com>
2021-08-29 22:00:03 -0300

    feat: add dot env file and removing sensible information from configs folder

--- src/configs/discordAuth.ts
+++
@@ -1,6 +0,0 @@
-export const REDIRECT_URI =
-    "https://auth.expo.io/@anonymous/gameplay-fb2d9f3b-071a-4f80-8bd1-916ff6fba9fb";
-export const SCOPE = "identify%20email%20connections%20guilds";
-export const RESPONSE_TYPE = "token";
-export const CLIENT_ID = "881667202657419334";
-export const CDN_IMAGE = "https://cdn.discordapp.com";
