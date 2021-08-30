15f4ec9266de2a88ec5d1e1594d48fd47da4505d
Andre Flor <andreflor96@hotmail.com>
2021-08-29 21:24:05 -0300

    fix: using user info from provider

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,20 +1,22 @@
 import React from "react";
 
 import { Text, View } from "react-native";
+import { useAuth } from "../../hooks/auth";
 import { Avatar } from "../Avatar";
 import { styles } from "./styles";
 
 export const Profile = () => {
-  return (
-    <View style={styles.container}>
-      <Avatar urlImage="https://github.com/andreflor21.png" />
-      <View>
-        <View style={styles.user}>
-          <Text style={styles.greeting}>Olá,</Text>
-          <Text style={styles.username}>André</Text>
+    const { user } = useAuth();
+    return (
+        <View style={styles.container}>
+            <Avatar urlImage={user.avatar} />
+            <View>
+                <View style={styles.user}>
+                    <Text style={styles.greeting}>Olá,</Text>
+                    <Text style={styles.username}>{user.firstName}</Text>
+                </View>
+                <Text style={styles.message}>Hoje é dia de vitória</Text>
+            </View>
         </View>
-        <Text style={styles.message}>Hoje é dia de vitória</Text>
-      </View>
-    </View>
-  );
+    );
 };
