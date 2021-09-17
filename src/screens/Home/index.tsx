2e2a3e66f2866f95b8f32fe8bfe616186dfb830a
Andre Flor <andreflor96@hotmail.com>
2021-09-17 16:23:58 -0300

    feat: add appointments dinamicaly

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,85 +1,99 @@
-import React, { useState } from "react";
-import { useNavigation } from "@react-navigation/native";
+import React, { useState, useCallback } from "react";
+import { View, FlatList } from "react-native";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import { FlatList, Text, View } from "react-native";
-import { Profile } from "../../components/Profile";
+import { CategorySelect } from "../../components/CategorySelect";
+import { Appointment, AppointmentProps } from "../../components/Appointment";
+import { ListDivider } from "../../components/ListDivider";
+import { ListHeader } from "../../components/ListHeader";
 import { Background } from "../../components/Background";
+import { ButtonAdd } from "../../components/ButtonAdd";
+import { Profile } from "../../components/Profile";
+import { Load } from "../../components/Load";
 
 import { styles } from "./styles";
-import { ButtonAdd } from "../../components/ButtonAdd";
-import { CategorySelect } from "../../components/CategorySelect";
-import { ListHeader } from "../../components/ListHeader";
-import { Appointment } from "../../components/Appointment";
-import { ListDivider } from "../../components/ListDivider";
-export function Home() {
-    const [category, setCategory] = useState<string>("");
+import { COLLECTION_APPOINTMENTS } from "../../configs/storage";
+
+export const Home = () => {
+    const [category, setCategory] = useState("");
+    const [loading, setLoading] = useState(true);
+    const [appointments, setAppointments] = useState<AppointmentProps[]>([]);
+
     const navigation = useNavigation();
-    const appointments = [
-        {
-            id: "1",
-            guild: {
-                id: "1",
-                name: "lendarios",
-                icon: null,
-                owner: true,
-            },
-            category: "1",
-            date: "22/06 as 20:40h",
-            description:
-                "É hoje que vamos chegar ao challenger sem perder uma partida na md10",
-        },
-        {
-            id: "2",
-            guild: {
-                id: "1",
-                name: "lendarios",
-                icon: null,
-                owner: true,
-            },
-            category: "1",
-            date: "22/06 as 20:40h",
-            description:
-                "É hoje que vamos chegar ao challenger sem perder uma partida na md10",
-        },
-    ];
+
     const handleCategorySelect = (categoryId: string) => {
         categoryId === category ? setCategory("") : setCategory(categoryId);
     };
-    const handleAppointmentDetails = () => {
-        navigation.navigate("AppointmentDetails");
+
+    const handleAppointmentDetails = (guildSelected: AppointmentProps) => {
+        navigation.navigate("AppointmentDetails", { guildSelected });
     };
+
     const handleAppointmentCreate = () => {
         navigation.navigate("AppointmentCreate");
     };
+
+    const loadAppointments = async () => {
+        const response = await AsyncStorage.getItem(COLLECTION_APPOINTMENTS);
+        const storage: AppointmentProps[] = response
+            ? JSON.parse(response)
+            : [];
+
+        if (category) {
+            setAppointments(
+                storage.filter((item) => item.category === category)
+            );
+        } else {
+            setAppointments(storage);
+        }
+
+        setLoading(false);
+    };
+
+    useFocusEffect(
+        useCallback(() => {
+            loadAppointments();
+        }, [category])
+    );
+
     return (
         <Background>
-            <View>
-                <View style={styles.header}>
-                    <Profile />
-                    <ButtonAdd onPress={handleAppointmentCreate} />
-                </View>
-                <CategorySelect
-                    categorySelected={category}
-                    setCategory={handleCategorySelect}
-                />
-
-                <ListHeader title="Partidas Agendadas" subtitle="Total 6" />
-
-                <FlatList
-                    data={appointments}
-                    keyExtractor={(item) => item.id}
-                    renderItem={({ item }) => (
-                        <Appointment
-                            onPress={handleAppointmentDetails}
-                            data={item}
-                        />
-                    )}
-                    ItemSeparatorComponent={() => <ListDivider />}
-                    contentContainerStyle={{ paddingBottom: 69 }}
-                    style={styles.matches}
-                    showsVerticalScrollIndicator={false}
-                />
+            <View style={styles.header}>
+                <Profile />
+                <ButtonAdd onPress={handleAppointmentCreate} />
             </View>
+
+            <CategorySelect
+                categorySelected={category}
+                setCategory={handleCategorySelect}
+            />
+
+            {loading ? (
+                <Load />
+            ) : (
+                <>
+                    <ListHeader
+                        title="Partidas agendadas"
+                        subtitle={`Total ${appointments.length}`}
+                    />
+
+                    <FlatList
+                        data={appointments}
+                        keyExtractor={(item) => item.id}
+                        renderItem={({ item }) => (
+                            <Appointment
+                                data={item}
+                                onPress={() => handleAppointmentDetails(item)}
+                            />
+                        )}
+                        ItemSeparatorComponent={() => <ListDivider />}
+                        contentContainerStyle={{ paddingBottom: 69 }}
+                        style={styles.matches}
+                        showsVerticalScrollIndicator={false}
+                    />
+                </>
+            )}
         </Background>
     );
-}
+};
