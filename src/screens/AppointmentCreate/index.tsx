bf0031a5eb7eff27c3c2c4149b92f9a5d968c7da
Andre Flor <andreflor96@hotmail.com>
2021-09-17 15:56:46 -0300

    feat: get guilds dinamicaly

--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -1,43 +1,83 @@
 import React, { useState } from "react";
+import { Feather } from "@expo/vector-icons";
+import { RectButton } from "react-native-gesture-handler";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useNavigation } from "@react-navigation/native";
+import uuid from "react-native-uuid";
+
 import {
-    View,
     Text,
+    View,
+    Platform,
     ScrollView,
     KeyboardAvoidingView,
-    Platform,
 } from "react-native";
-import { RectButton } from "react-native-gesture-handler";
-import { Feather } from "@expo/vector-icons";
+
+import { COLLECTION_APPOINTMENTS } from "../../configs/storage";
 import { theme } from "../../global/styles/theme";
 import { styles } from "./styles";
 
-import { Background } from "../../components/Background";
 import { CategorySelect } from "../../components/CategorySelect";
-import { Header } from "../../components/Header";
-import { GuildIcon } from "../../components/GuildIcon";
-import { SmallInput } from "../../components/SmallInput";
-import { TextArea } from "../../components/TextArea";
-import { Button } from "../../components/Button";
 import { ModalView } from "../../components/ModalView";
-import { Guilds } from "../Guilds";
+import { Background } from "../../components/Background";
+import { SmallInput } from "../../components/SmallInput";
+import { GuildIcon } from "../../components/GuildIcon";
+import { TextArea } from "../../components/TextArea";
 import { GuildProps } from "../../components/Guild";
-export const AppointmentCreate = () => {
-    const [category, setCategory] = useState<string>("");
-    const [openGuildsModal, setOpenGuildsModal] = useState<boolean>(false);
+import { Header } from "../../components/Header";
+import { Button } from "../../components/Button";
+import { Guilds } from "../Guilds";
+
+export function AppointmentCreate() {
+    const [category, setCategory] = useState("");
+    const [openGuildsModa, setOpenGuildsModal] = useState(false);
     const [guild, setGuild] = useState<GuildProps>({} as GuildProps);
-    const handleOpenGuilds = () => {
+
+    const [day, setDay] = useState("");
+    const [month, setMonth] = useState("");
+    const [hour, setHour] = useState("");
+    const [minute, setMinute] = useState("");
+    const [description, setDescription] = useState("");
+
+    const navigation = useNavigation();
+
+    function handleOpenGuilds() {
         setOpenGuildsModal(true);
-    };
-    const handleCloseGuilds = () => {
+    }
+
+    function handleCloseGuilds() {
         setOpenGuildsModal(false);
-    };
-    const handleGuildSelect = (guildSelected: GuildProps) => {
-        setGuild(guildSelected);
+    }
+
+    function handleGuildSelect(guildSelect: GuildProps) {
+        setGuild(guildSelect);
         setOpenGuildsModal(false);
-    };
-    const handleCategorySelect = (categoryId: string) => {
+    }
+
+    function handleCategorySelect(categoryId: string) {
         setCategory(categoryId);
-    };
+    }
+
+    async function handleSave() {
+        const newAppointment = {
+            id: uuid.v4(),
+            guild,
+            category,
+            date: `${day}/${month} às ${hour}:${minute}h`,
+            description,
+        };
+
+        const storage = await AsyncStorage.getItem(COLLECTION_APPOINTMENTS);
+        const appointments = storage ? JSON.parse(storage) : [];
+
+        await AsyncStorage.setItem(
+            COLLECTION_APPOINTMENTS,
+            JSON.stringify([...appointments, newAppointment])
+        );
+
+        navigation.navigate("Home");
+    }
+
     return (
         <KeyboardAvoidingView
             behavior={Platform.OS === "ios" ? "padding" : "height"}
@@ -45,7 +85,7 @@ export const AppointmentCreate = () => {
         >
             <Background>
                 <ScrollView>
-                    <Header title="Agendar Partida" />
+                    <Header title="Agendar partida" />
 
                     <Text
                         style={[
@@ -55,19 +95,25 @@ export const AppointmentCreate = () => {
                     >
                         Categoria
                     </Text>
+
                     <CategorySelect
                         hasCheckBox
                         setCategory={handleCategorySelect}
                         categorySelected={category}
                     />
+
                     <View style={styles.form}>
                         <RectButton onPress={handleOpenGuilds}>
                             <View style={styles.select}>
                                 {guild.icon ? (
-                                    <GuildIcon />
+                                    <GuildIcon
+                                        guildId={guild.id}
+                                        iconId={guild.icon}
+                                    />
                                 ) : (
                                     <View style={styles.image} />
                                 )}
+
                                 <View style={styles.selectBody}>
                                     <Text style={styles.label}>
                                         {guild.name
@@ -75,6 +121,7 @@ export const AppointmentCreate = () => {
                                             : "Selecione um servidor"}
                                     </Text>
                                 </View>
+
                                 <Feather
                                     name="chevron-right"
                                     color={theme.colors.heading}
@@ -82,18 +129,25 @@ export const AppointmentCreate = () => {
                                 />
                             </View>
                         </RectButton>
+
                         <View style={styles.field}>
                             <View>
                                 <Text
                                     style={[styles.label, { marginBottom: 12 }]}
                                 >
-                                    {" "}
                                     Dia e mês
                                 </Text>
+
                                 <View style={styles.column}>
-                                    <SmallInput maxLength={2} />
+                                    <SmallInput
+                                        maxLength={2}
+                                        onChangeText={setDay}
+                                    />
                                     <Text style={styles.divider}>/</Text>
-                                    <SmallInput maxLength={2} />
+                                    <SmallInput
+                                        maxLength={2}
+                                        onChangeText={setMonth}
+                                    />
                                 </View>
                             </View>
 
@@ -101,37 +155,49 @@ export const AppointmentCreate = () => {
                                 <Text
                                     style={[styles.label, { marginBottom: 12 }]}
                                 >
-                                    {" "}
                                     Hora e minuto
                                 </Text>
+
                                 <View style={styles.column}>
-                                    <SmallInput maxLength={2} />
+                                    <SmallInput
+                                        maxLength={2}
+                                        onChangeText={setHour}
+                                    />
                                     <Text style={styles.divider}>:</Text>
-                                    <SmallInput maxLength={2} />
+                                    <SmallInput
+                                        maxLength={2}
+                                        onChangeText={setMinute}
+                                    />
                                 </View>
                             </View>
                         </View>
+
                         <View style={[styles.field, { marginBottom: 12 }]}>
                             <Text style={styles.label}>Descrição</Text>
+
                             <Text style={styles.caracteresLimit}>
                                 Max 100 caracteres
                             </Text>
                         </View>
+
                         <TextArea
                             multiline
                             maxLength={100}
                             numberOfLines={5}
                             autoCorrect={false}
+                            onChangeText={setDescription}
                         />
+
                         <View style={styles.footer}>
-                            <Button title="Agendar" />
+                            <Button title="Agendar" onPress={handleSave} />
                         </View>
                     </View>
                 </ScrollView>
             </Background>
-            <ModalView visible={openGuildsModal} closeModal={handleCloseGuilds}>
+
+            <ModalView visible={openGuildsModa} closeModal={handleCloseGuilds}>
                 <Guilds handleGuildSelect={handleGuildSelect} />
             </ModalView>
         </KeyboardAvoidingView>
     );
-};
+}
