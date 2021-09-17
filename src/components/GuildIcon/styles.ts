da5dd725b8c12bc24c16c52e8001fea8fddb0510
Andre Flor <andreflor96@hotmail.com>
2021-09-17 13:25:59 -0300

    fix: use guild icon from discord api

--- src/components/GuildIcon/styles.ts
+++ src/components/GuildIcon/styles.ts
@@ -4,6 +4,12 @@ export const styles = StyleSheet.create({
     image: {
         width: 62,
         height: 66,
+    },
+    container: {
+        width: 62,
+        height: 66,
         borderRadius: 8,
+        justifyContent: "center",
+        alignItems: "center",
     },
 });
